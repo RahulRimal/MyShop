e093c7e494cc7b260dc41f51d553c8d8e4b6e9c1
Rahul Rimal <genuinetechnology9@gmail.com>
2021-07-16 13:00:45 +0545

    Adding Listeners to the provider

---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:myshop/providers/products.dart';
+import 'package:myshop/widgets/product_item.dart';
+import 'package:provider/provider.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.items;
+
+    return GridView.builder(
+      itemCount: products.length,
+      padding: const EdgeInsets.all(10),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10),
+      itemBuilder: (context, index) => ProductItem(
+        products[index].id,
+        products[index].title,
+        products[index].imageUrl,
+      ),
+    );
+  }
+}
