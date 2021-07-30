340e1722968437048bde23fec1b17b6f49f66e36
Rahul Rimal <genuinetechnology9@gmail.com>
2021-07-30 15:40:24 +0545

    Image Input and Preview Completed

---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:myshop/providers/products.dart';
+import 'package:myshop/screens/edit_product_screen.dart';
+import 'package:myshop/widgets/app_drawer.dart';
+import 'package:myshop/widgets/user_product_item.dart';
+import 'package:provider/provider.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+
+  @override
+  Widget build(BuildContext context) {
+    final productData = Provider.of<Products>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Products'),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                Navigator.of(context).pushNamed(EditProductScreen.routName);
+              })
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+          itemCount: productData.items.length,
+          itemBuilder: (context, index) => Column(
+            children: [
+              UserProductItem(
+                productData.items[index].title,
+                productData.items[index].imageUrl,
+              ),
+              Divider(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
